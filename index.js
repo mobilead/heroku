b68c4df5dfc09e6e9315168b6a4b63264bb2512a
unknown <heroku@mobilead.fr>
2015-06-17 14:50:07 +0200

    index test phantom

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+/*
 app.get('/', function(request, response) {
   response.send('Hellhoh!');
 });
+*/
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+var path = require('path');
+var childProcess = require('child_process');
+var phantomjs = require('phantomjs');
+var binPath = phantomjs.path;
+ 
+var childArgs = [
+  path.join(__dirname, 'phantomjs-script.js'),
+  'some other argument (passed to phantomjs script)'
+];
+ 
+childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+  // handle results 
 });
