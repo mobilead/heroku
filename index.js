c484d43618b2909e16b8c66f83822fbdc12900da
unknown <heroku@mobilead.fr>
2015-06-17 15:15:53 +0200

    phantom script index

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-/*
-app.get('/', function(request, response) {
-  response.send('Hellhoh!');
-});
-*/
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
@@ -26,4 +21,8 @@ var childArgs = [
  
 childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
   // handle results 
+});
+
+app.get('/', function(request, response) {
+  response.send('FIN!');
 });
